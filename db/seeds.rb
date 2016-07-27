2f66c0f3214d4c07301ea1b24d3a70bc679513ce
Graham Ormiston <grahamormiston@gmail.com>
2016-07-27 15:15:22 +0100

    Added placeholder users to seed.db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+[
+  { name: "Anna",   email: "anna@example.com"},
+  { name: "Tim", email: "tim@example.com"},
+  { name: "Alistair",  email: "alistair@example.com"},
+  { name: "Enzo", email: "enzo@example.com"},
+  { name: "Graham", email: "graham@example.com"},
+  { name: "Lukasz",   email: "lukasz@example.com"}
+].each do |user_attributes|
+  User.create(user_attributes)
+end
