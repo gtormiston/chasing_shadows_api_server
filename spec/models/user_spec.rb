82dfbb1b03e8ee01044d6f9031abbbd8c6fb1f11
Graham Ormiston <grahamormiston@gmail.com>
2016-07-27 17:03:14 +0100

    Updated tests for password auth

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,8 +7,7 @@ RSpec.describe User, type: :model do
     it { is_expected.to have_db_column(:email).of_type(:string) }
     it { is_expected.to have_db_column(:created_at).of_type(:datetime) }
     it { is_expected.to have_db_column(:updated_at).of_type(:datetime) }
-    it { is_expected.to have_db_column(:password).of_type(:string) }
-    it { is_expected.to have_db_column(:password_confirmation).of_type(:string) }
+    it { is_expected.to have_db_column(:password_digest).of_type(:string) }
     it { is_expected.to have_db_column(:api_key).of_type(:string) }
   end
 
