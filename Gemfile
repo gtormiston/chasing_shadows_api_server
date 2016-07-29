cacc8f72f58ce824e9d8515d2f0e1a39226e4d2a
Anna <anna.hollandsmith3@gmail.com>
2016-07-29 15:36:55 +0100

    added simplecov

--- Gemfile
+++ Gemfile
@@ -14,6 +14,7 @@ group :development, :test do
   gem 'rspec-rails'
   gem 'factory_girl_rails'
   gem 'shoulda-matchers'
+  gem 'simplecov', :require => false, :group => :test
 end
 
 group :development do
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,7 +6,10 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 require 'spec_helper'
 require 'rspec/rails'
 require 'shoulda/matchers'
+require 'simplecov'
+SimpleCov.start
 require 'test_helper'
+
 
 # Add additional requires below this line. Rails is not loaded until this point!
 
