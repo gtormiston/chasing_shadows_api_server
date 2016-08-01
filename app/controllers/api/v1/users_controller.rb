275602663f3c17193460714e00c1e3ed50f42f6e
Crescenzo D'Alterio <crescenzo.dalterio@gmail.com>
2016-08-01 17:23:14 +0100

    debugging ajax 2

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -17,7 +17,7 @@ module Api::V1
     end
 
     def update
-      p request.headers
+      p request.headers["HTTP_USER_LOCATION"]
       if @current_user.update(lat: request.headers["HTTP_USER_LOCATION"]["lat"],
                               lng: request.headers["HTTP_USER_LOCATION"]["lng"]  )
       else
